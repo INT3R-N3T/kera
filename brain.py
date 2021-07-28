531870149996e77035a7c2295ac72c42264986c8
INT3R-N3T <64934401+INT3R-N3T@users.noreply.github.com>
2021-07-27 20:20:55 -0700

    added greeting

--- brain.py
+++ brain.py
@@ -1,10 +1,40 @@
+"""
+My goals for the brain is that it will be able to sit and wait for a hello cmd.
+then i want it to give a greeting. after that it will listen for cmd words. 
+and then use other moduels to follow through with that cmd.
+
+"""
+
+
+
+
+
+
 import os
+import time
+import json
 
 from ear import *
 from mouth import *
 
 
-# this takes voice from ear and converts it into string
-listen = Voicetotext(mic, r)
 
-speak(listen)
+
+while True:
+
+
+    # this takes voice from ear and converts it into string
+    greetingRequest = Voicetotext(mic, r)
+
+    # This if trys to find hello and key
+    if (greetingRequest.find("hello") != -1) & (greetingRequest.find("Kira") != -1):
+        
+        # will speak the greeting
+        speak("Hey Dom, how are you today")
+        break
+    else:
+        time.sleep(1)
+        continue
+
+
+
